b6d4a8be862b4385a3802b51ef1143274c33f1eb
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-08-19 10:38:12 +0800

    remove dist floder

--- dist/services/ComputationService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ComputationService = /** @class */ (function () {
-    function ComputationService() {
-    }
-    return ComputationService;
-}());
-exports.default = ComputationService;
