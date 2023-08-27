428bd92b70ad22447f597559c41a970ced105bcf
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-08-27 13:11:43 +0800

    feat: create unit tests

---
+++ src/__tests__/services/ConversionService.spec.ts
@@ -0,0 +1,15 @@
+import ConversionService from "../../services/ConversionService";
+
+describe("/services/ConversionService.ts", () => {
+  test("a. binaryToDecimal method should return the right value.", (done) => {
+    const binaryToDecimalTest = ConversionService.binaryToDecimal("1011")
+    expect(binaryToDecimalTest).toEqual(11)
+    done()
+  })
+
+  test("b. decimalToBinary method should return the right value.", (done) => {
+    const decimalToBinaryTest = ConversionService.decimalToBinary("11")
+    expect(decimalToBinaryTest).toEqual(1011)
+    done()
+  })
+})
